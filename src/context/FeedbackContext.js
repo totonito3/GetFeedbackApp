986636f4c25c739bfd46f617f097ae26a1499acf
Bertin Tamegnon <btamegnon3@gmail.com>
2023-09-08 19:30:13 -0400

    Added Update functionality

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -21,6 +21,11 @@ export const FeedbackProvider = ({ children }) => {
       rating: 1,
     },
   ]);
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete? ")) {
       setFeedback(feedback.filter((item) => item.id !== id));
@@ -32,12 +37,29 @@ export const FeedbackProvider = ({ children }) => {
     console.log(newFeedback.id);
     setFeedback([newFeedback, ...feedback]);
   };
+
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
+
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
+        editFeedback,
+        updateFeedback,
       }}>
       {children}
     </FeedbackContext.Provider>
